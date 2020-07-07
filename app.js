72072a38dfb70de16f85f2df47a3f90e43d26230
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2020-07-07 12:07:29 +0530

    update projects dashboard page

---
+++ app.js
@@ -0,0 +1,8 @@
+import projects from './projects.js';
+
+new Vue({
+    el: '#vue-app',
+    data: {
+        projects
+    }
+});
---
+++ projects.js
@@ -0,0 +1,20 @@
+const projects = [
+    {
+        id: 1,
+        title: 'Georgia Restaurant',
+        technologies: ['HTML', 'CSS', 'SASS'],
+        designUrl: './designs/georgia-restaurant.jpg',
+        url: './georgia-restaurant/index.html'
+        
+    },
+    {
+        id: 2,
+        title: 'clothing store',
+        technologies: ['HTML', 'CSS', 'SASS'],
+        designUrl: './designs/clothes-store.jpg',
+        url: './clothing-store/index.html'
+        
+    }
+]
+
+export default projects;
